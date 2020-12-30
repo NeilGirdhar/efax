e2d062b9874a9aa7a1cab477c8bc39560c3e77d7
Neil Girdhar <mistersheik@gmail.com>
2020-12-29 23:35:41 -0500

    Use tensorflow_probability.lbfgs_minimize

--- efax/exp_to_nat.py
+++ efax/exp_to_nat.py
@@ -1,14 +1,17 @@
 from __future__ import annotations
 
-from typing import Any, Generic, List, Optional, Tuple, TypeVar
+from functools import partial
+from typing import Any, Generic, List, TypeVar
 
 from chex import Array
+from jax import jit
 from jax import numpy as jnp
+from jax import value_and_grad, vmap
 from jax.tree_util import tree_multimap
-from tjax import field_names_and_values, dataclass
-from tjax.fixed_point import ComparingIteratedFunctionWithCombinator, ComparingState
+from tensorflow_probability.substrates.jax.optimizer import lbfgs_minimize
+from tjax import field_names_and_values
 
-from .exponential_family import ExpectationParametrization, NaturalParametrization
+from .exponential_family import ExpectationParametrization, NaturalParametrization, tree_dot_final
 
 __all__: List[str] = []
 
@@ -24,10 +27,25 @@ class ExpToNat(ExpectationParametrization[NP], Generic[NP]):
 
     # Implemented methods --------------------------------------------------------------------------
     def to_nat(self) -> NP:
-        iterated_function = ExpToNatIteratedFunction[NP](minimum_iterations=1000,
-                                                         maximum_iterations=1000)
-        state = iterated_function.find_fixed_point(self, self.initial_natural()).current_state
-        return self.transform_natural_for_iteration(state)
+        nat_cls = self.natural_parametrization_cls()
+
+        @jit
+        def f(target: ExpToNat[NP], x: Array) -> Array:
+            state = nat_cls.unflattened(x)
+            natural_parameters = target.transform_natural_for_iteration(state)
+            return -tree_dot_final(natural_parameters, self) + natural_parameters.log_normalizer()
+            expectation_parameters = natural_parameters.to_exp()
+            exp_difference = tree_multimap(jnp.subtract, expectation_parameters, target)
+            retval = jnp.sum(jnp.square(exp_difference.flattened()), axis=-1)
+            return retval
+
+        f = vmap((value_and_grad(f, argnums=1)))
+        fp = partial(f, self)
+
+        results = lbfgs_minimize(fp, self.initial_natural().flattened(),
+                                 tolerance=1e-5)
+        natural_parameters = nat_cls.unflattened(results.position)
+        return self.transform_natural_for_iteration(natural_parameters)
 
     # Non-final methods ----------------------------------------------------------------------------
     def initial_natural(self) -> NP:
@@ -35,8 +53,8 @@ class ExpToNat(ExpectationParametrization[NP], Generic[NP]):
         Returns: A form of the natural parameters that Newton's method will run on.
         """
         kwargs = dict(field_names_and_values(self, static=True))
-        cls = type(self).natural_parametrization_cls()
-        return cls.unflattened(jnp.zeros_like(self.flattened()), **kwargs)
+        nat_cls = type(self).natural_parametrization_cls()
+        return nat_cls.unflattened(jnp.zeros_like(self.flattened()), **kwargs)
 
     @classmethod
     def transform_natural_for_iteration(cls, iteration_natural: NP) -> NP:
@@ -46,46 +64,3 @@ class ExpToNat(ExpectationParametrization[NP], Generic[NP]):
         Returns: The corresponding natural parametrization.
         """
         raise NotImplementedError
-
-
-@dataclass
-class ExpToNatIteratedFunction(
-        # The generic parameters are: Parameters, State, Comparand, Differentiand, Trajectory.
-        ComparingIteratedFunctionWithCombinator[ExpToNat[NP],
-                                                NP,
-                                                ExpToNat[NP],
-                                                NP,
-                                                NP],
-        Generic[NP]):
-
-    def sampled_state(self, theta: ExpToNat[NP], state: NP) -> NP:
-        def scaled_difference(target: Array, guess: Array) -> Array:
-            return 1e-1 * (guess - target)
-
-        def delta(theta: ExpToNat[NP], state: NP) -> NP:
-            natural_parameters = theta.transform_natural_for_iteration(state)
-            expectation_parameters = natural_parameters.to_exp()
-            exp_difference = tree_multimap(scaled_difference, theta, expectation_parameters)
-            nat_cls = type(state)
-            return nat_cls.unflattened(exp_difference.flattened())
-        # TODO: use jacobian
-
-        # Adjust state in the direction of delta.
-        the_delta = delta(theta, state)
-        return tree_multimap(jnp.subtract, state, the_delta)
-
-    def sampled_state_trajectory(self,
-                                 theta: ExpToNat[NP],
-                                 augmented: ComparingState[NP, ExpToNat[NP]]) -> Tuple[NP, NP]:
-        sampled_state = self.sampled_state(theta, augmented.current_state)
-        trajectory = sampled_state
-        return sampled_state, trajectory
-
-    def extract_comparand(self, state: NP) -> ExpToNat[NP]:
-        return state.to_exp()
-
-    def extract_differentiand(self, state: NP) -> NP:
-        return state
-
-    def implant_differentiand(self, state: NP, differentiand: NP) -> NP:
-        return differentiand
