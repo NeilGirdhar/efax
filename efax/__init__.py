2b2fe13ab2965bc5729b933c2da53e32438f1878
Neil Girdhar <mistersheik@gmail.com>
2021-04-12 15:37:58 -0400

    Hide module and package names from public interface

--- efax/__init__.py
+++ efax/__init__.py
@@ -1,8 +1,16 @@
-from .conjugate_prior import *
-from .distributions import *
-from .expectation_parametrization import *
-from .natural_parametrization import *
-from .parameter import *
-from .samplable import *
-from .scipy_replacement import *
-from .tools import *
+from ._src import (ExpectationParametrization, HasConjugatePrior, NaturalParametrization, Samplable,
+                   ScalarSupport, SquareMatrixSupport, Support, SymmetricMatrixSupport,
+                   VectorSupport, parameters_name_support, parameters_name_value_support,
+                   parameters_value_support)
+from ._src.distributions import (BernoulliEP, BernoulliNP, BetaEP, BetaNP, ChiEP, ChiNP,
+                                 ChiSquareEP, ChiSquareNP, ComplexNormalEP, ComplexNormalNP,
+                                 DirichletEP, DirichletNP, ExponentialEP, ExponentialNP, GammaEP,
+                                 GammaNP, GeometricEP, GeometricNP, IsotropicNormalEP,
+                                 IsotropicNormalNP, LogarithmicEP, LogarithmicNP, MultinomialEP,
+                                 MultinomialNP, MultivariateNormalEP, MultivariateNormalNP,
+                                 MultivariateNormalVP, MultivariateUnitNormalEP,
+                                 MultivariateUnitNormalNP, NegativeBinomialEP, NegativeBinomialNP,
+                                 NormalEP, NormalNP, PoissonEP, PoissonNP, RayleighEP, RayleighNP,
+                                 VonMisesFisherEP, VonMisesFisherNP)
+from ._src.scipy_replacement import (ScipyComplexMultivariateNormal, ScipyComplexNormal,
+                                     ScipyDirichlet, ScipyMultivariateNormal)
