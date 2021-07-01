92a45e48778470673354e8f369ca52d0fbb47dfe
Neil Girdhar <mistersheik@gmail.com>
2021-07-01 12:12:31 -0400

    Polish

--- efax/_src/scipy_replacement/multivariate_normal.py
+++ efax/_src/scipy_replacement/multivariate_normal.py
@@ -9,11 +9,10 @@ __all__ = ['ScipyMultivariateNormal']
 
 # pylint: disable=protected-access
 class ScipyMultivariateNormal(ss._multivariate.multivariate_normal_frozen):
-
     def rvs(self,
             size: Optional[Shape] = None,
-            generator: Optional[Generator] = None) -> RealArray:
-        retval = super().rvs(size=size, random_state=generator)
+            random_state: Optional[Generator] = None) -> RealArray:
+        retval = super().rvs(size=size, random_state=random_state)
         if size == (1,):
             assert False
             retval = retval.reshape((1, 2))
