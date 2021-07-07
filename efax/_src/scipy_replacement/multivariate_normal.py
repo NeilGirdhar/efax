72e162330e9ce8e7a5659bcbbcd1db62b03a05e9
Neil Girdhar <mistersheik@gmail.com>
2021-07-07 08:25:36 -0400

    Repair ScipyMultivariateNormal().rvs((1,))

--- efax/_src/scipy_replacement/multivariate_normal.py
+++ efax/_src/scipy_replacement/multivariate_normal.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 from numpy.random import Generator
 from scipy import stats as ss
-from tjax import RealArray, Shape
+from tjax import RealArray, ShapeLike
 
 __all__ = ['ScipyMultivariateNormal']
 
@@ -10,10 +10,13 @@ __all__ = ['ScipyMultivariateNormal']
 # pylint: disable=protected-access
 class ScipyMultivariateNormal(ss._multivariate.multivariate_normal_frozen):
     def rvs(self,
-            size: Optional[Shape] = None,
+            size: Optional[ShapeLike] = None,
             random_state: Optional[Generator] = None) -> RealArray:
         retval = super().rvs(size=size, random_state=random_state)
-        if size == (1,):
-            assert False
-            retval = retval.reshape((1, 2))
-        return retval
+        if size is None:
+            size = ()
+        elif isinstance(size, int):
+            size = (size,)
+        else:
+            size = tuple(size)
+        return retval.reshape(size + self.mean.shape)
