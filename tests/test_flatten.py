8f36cd81abfcdaaaed38b90cec433f8aec3e75e0
Neil Girdhar <mistersheik@gmail.com>
2024-05-13 20:19:30 -0400

    Simplify test_flatten

--- tests/test_flatten.py
+++ tests/test_flatten.py
@@ -3,29 +3,25 @@ from __future__ import annotations
 from typing import Any
 
 import numpy as np
+import pytest
 from numpy.random import Generator
 from tjax import assert_tree_allclose
 
 from .distribution_info import DistributionInfo
 
 
+@pytest.mark.parametrize('natural', [False, True])
 def test_flatten(generator: Generator,
-                 distribution_info: DistributionInfo[Any, Any, Any]
+                 distribution_info: DistributionInfo[Any, Any, Any],
+                 *,
+                 natural: bool,
                  ) -> None:
     """Test that the methods produce the correct shapes."""
     shape = (3, 4)
-
-    p = distribution_info.exp_parameter_generator(generator, shape=shape)
-    q = distribution_info.nat_parameter_generator(generator, shape=shape)
-
+    p = (distribution_info.exp_parameter_generator(generator, shape=shape)
+         if natural
+         else distribution_info.nat_parameter_generator(generator, shape=shape))
     p_kwargs = p.fixed_parameters()
-    q_kwargs = q.fixed_parameters()
-
     p_flat = p.flattened()
-    q_flat = q.flattened()
-
     assert issubclass(p_flat.dtype.type, np.floating)
-    assert issubclass(q_flat.dtype.type, np.floating)
-
     assert_tree_allclose(type(p).unflattened(p_flat, **p_kwargs), p)
-    assert_tree_allclose(type(q).unflattened(q_flat, **q_kwargs), q)
