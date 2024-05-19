91b27ce4fc41111c08e19d4dacbff42c89a74da2
Neil Girdhar <mistersheik@gmail.com>
2024-05-19 04:31:11 -0400

    Raise exception for bad flatten input

--- tests/test_flatten.py
+++ tests/test_flatten.py
@@ -2,12 +2,13 @@ from __future__ import annotations
 
 from typing import Any
 
+import jax.numpy as jnp
 import numpy as np
 import pytest
 from numpy.random import Generator
 from tjax import assert_tree_allclose
 
-from efax import Flattener
+from efax import Flattener, MultivariateUnitNormalNP
 
 from .distribution_info import DistributionInfo
 
@@ -18,7 +19,7 @@ def test_flatten(generator: Generator,
                  *,
                  natural: bool,
                  ) -> None:
-    """Test that the methods produce the correct shapes."""
+    """Test that unflattening restores the source of flattening."""
     shape = (3, 4)
     p = (distribution_info.exp_parameter_generator(generator, shape=shape)
          if natural
@@ -26,3 +27,14 @@ def test_flatten(generator: Generator,
     flattener, flattened = Flattener.flatten(p)
     assert issubclass(flattened.dtype.type, np.floating)
     assert_tree_allclose(flattener.unflatten(flattened), p)
+
+
+def test_raises() -> None:
+    """Test that unflattening raises for bad shapes."""
+    m = MultivariateUnitNormalNP(jnp.zeros(10))
+    flattener, flattened = Flattener.flatten(m)
+    assert flattened.shape == (10,)
+    with pytest.raises(ValueError, match="Incompatible array"):
+        flattener.unflatten(jnp.zeros(5))
+    with pytest.raises(ValueError, match="Incompatible array"):
+        flattener.unflatten(jnp.zeros(12))
